42e18493cd624b0ca81435b9f889c87059a7c3ce
shivam <shivam.spirite3@outlook.com>
2023-05-03 10:10:41 +0530

    add movie review app

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import { Movie } from '../models/Movie';
 import { useState } from 'react';
 import { Box, Button, FormControl, FormLabel, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Textarea } from '@chakra-ui/react';
+import * as web3 from '@solana/web3.js'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 
 const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
 
@@ -10,6 +12,9 @@ export const Form: FC = () => {
     const [rating, setRating] = useState(0)
     const [message, setMessage] = useState('')
 
+    const {connection} = useConnection();
+    const {publicKey, sendTransaction} = useWallet()
+
     const handleSubmit = (event: any) => {
         event.preventDefault()
         const movie = new Movie(title, rating, message)
@@ -17,7 +22,50 @@ export const Form: FC = () => {
     }
 
     const handleTransactionSubmit = async (movie: Movie) => {
-        console.log(JSON.stringify(movie))
+        if (!publicKey) {
+            alert('Please connect your wallet!')
+            return
+        }
+
+        const buffer = movie.serialize()
+        const transaction = new web3.Transaction()
+        const [pda] = await web3.PublicKey.findProgramAddress(
+            [publicKey.toBuffer(), new TextEncoder().encode(movie.title)],
+            new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID)
+        )
+
+        const instruction = new web3.TransactionInstruction({
+            keys: [
+                {
+                    // Your account will pay the fees, so it's writing to the network
+                    pubkey: publicKey,
+                    isSigner: true,
+                    isWritable: false,
+                },
+                {
+                    // The PDA will store the movie review 
+                    pubkey: pda,
+                    isSigner: false,
+                    isWritable: true
+                },
+                {
+                    // The system program will be used for creating the PDA
+                    pubkey: web3.SystemProgram.programId,
+                    isSigner: false,
+                    isWritable: false
+                }
+            ],
+            data: buffer,
+            programId: new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID)
+        })
+       transaction.add(instruction);
+    
+        try {
+            let txid = await sendTransaction(transaction, connection)
+            console.log(`Transaction submitted: https://explorer.solana.com/tx/${txid}?cluster=devnet`)
+        } catch (e) {
+            alert(JSON.stringify(e))
+        }
     }
 
     return (
@@ -30,16 +78,16 @@ export const Form: FC = () => {
             justifyContent="center"
         >
             <form onSubmit={handleSubmit}>
-                <FormControl isRequired>
-                    <FormLabel color='gray.200'>
+             <FormControl isRequired>
+                <FormLabel color='gray.200'>
                         Movie Title
-                        </FormLabel>
+                </FormLabel>
                     <Input 
                     id='title' 
                     color='gray.400'
                     onChange={event => setTitle(event.currentTarget.value)}
                 />
-                </FormControl>
+             </FormControl>  
                 <FormControl isRequired>
                     <FormLabel color='gray.200'>
                         Add your review
